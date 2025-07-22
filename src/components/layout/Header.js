4a4b4912bd682c6614cdb4409d2902beb38f3994
akparthiban03 <parthi95664@gmail.com>
2025-07-22 22:14:10 +0530

    final comit

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -3,9 +3,17 @@ import { MdAutoGraph } from "react-icons/md";
 
 
 const Header = () => {
+
+  const handleLogout = () => {
+  localStorage.removeItem("isLoggedIn");
+  window.location.href = "/login";
+};
+
   return (
-    <div className="bg-white shadow h-10 w-full fixed top-0 left-0 z-20 flex items-center px-4 ">
+    <div className="bg-white shadow h-10 w-full fixed top-0 left-0 z-20 flex items-center justify-between px-4 ">
       <h1 className="text-xl font-semibold flex items-center gap-2">WorkNest <MdAutoGraph /></h1>
+      <button onClick={handleLogout} className="text-red-600 hover:underline ">Logout</button>
+
     </div>
   )
 }
